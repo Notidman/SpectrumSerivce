a4b6d98020999ff45c776c0d16ece1950dcb12d4
Egor Zorin <81178428+Notidman@users.noreply.github.com>
2022-01-25 00:46:57 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -3,12 +3,6 @@
 #include <thread>
 #include <chrono>
 
-#define debug_mode 0
-
-#if debug_mode
-
-#endif
-
 int main() {
     std::cout << "+-------------------------- Start --------------------------+" << std::endl;
     decltype(auto) connect{std::make_shared<sw::redis::ConnectionOptions>()};
@@ -19,23 +13,8 @@ int main() {
     decltype(auto) base{std::make_shared<rs::Base>("First", redis)};
     decltype(auto) spec{std::make_shared<rs::Spectrum<float>>("First", redis)};
 
-//-------------------------------------------------------------------------
-#if debug_mode
-    auto ws{[score = 1] (const char *str) mutable  {
-        std::cout << "[" << score++ << "] wait 4 sec" << " | " << str << std::endl;
-        std::this_thread::sleep_for(std::chrono::seconds(4));
-    }};
-#endif
-//-------------------------------------------------------------------------
-
-    __loop__
+    __loop__ // while(1)
     {
-//-------------------------------------------------------------------------
-#if debug_mode
-    ws("part1");
-#endif
-//-------------------------------------------------------------------------
-
         decltype(auto) request{base->listen()};
         if( request.has_value())
         {
@@ -43,18 +22,5 @@ int main() {
             base->execute_request(request);
             base->submit_reply();
         }
-
-//-------------------------------------------------------------------------
-#if debug_mode
-    ws("part2");
-#endif
-//-------------------------------------------------------------------------
-
-
-//-------------------------------------------------------------------------
-#if debug_mode
-    ws("part3");
-#endif
-//-------------------------------------------------------------------------
     };
 }
